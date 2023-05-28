70962f29d35d000a7d79072a0deec52576c0e2b9
belm960 <samuelbefikadu10@gmail.com>
2023-05-28 05:19:26 -0300

    header and seat choose

--- src/app/buslist/content/content.component.ts
+++ src/app/buslist/content/content.component.ts
@@ -10,25 +10,5 @@ export class ContentComponent {
 
   constructor(public dialog: MatDialog) {}
 
-  allComplete = true;
-  panelOpenState = true;
-  buses:  any []= [
-    {name: 'Odda', completed: false, color: 'primary'},
-    {name: 'Abay', completed: false, color: 'accent'},
-    {name: 'Ethio', completed: false, color: 'warn'},
-    {name: 'Geda', completed: false, color: 'warn'},
-    {name: 'Golden', completed: false, color: 'amber'},
-  ]
 
-  updateAllComplete() {
-    this.allComplete = this.buses != null && this.buses.every(t => t.completed);
-  }
-
-  openDialog() {
-    const dialogRef = this.dialog.open(SeatComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
 }
